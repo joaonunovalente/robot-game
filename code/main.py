867ea069b66bbb9d2e11a2222c7e248890cb374e
joaonunovalente <jnvalente@ua.pt>
2025-08-16 13:29:51 +0100

    Add a class for each image object

--- code/main.py
+++ code/main.py
@@ -36,10 +36,57 @@ class RobotGame:
 
     def draw_window(self):
         self.window.fill((230, 230, 230))
-
-
-
+        self.draw_objects()
         pygame.display.flip()
+
+    def draw_objects(self):
+        self.robot = Robot(200, 200)
+        self.robot.draw(self.window)
+
+        self.coin = Coin(300, 300)
+        self.coin.draw(self.window)
+
+        self.monster = Monster(400, 400)
+        self.monster.draw(self.window)
+
+        self.door = Door(500, 500)
+        self.door.draw(self.window)
+
+class Robot:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("assets/robot.png")
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
+
+class Coin:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("assets/coin.png")
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
+
+class Monster:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("assets/monster.png")
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
+
+class Door:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("assets/door.png")
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
 
 if __name__ == "__main__":
     RobotGame()
