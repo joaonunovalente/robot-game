8262331c6ece30147af847937116a894b2545d5b
joaonunovalente <jnvalente@ua.pt>
2025-08-16 16:30:36 +0100

    Add classes for Coin, Door, Monster, and Robot; update .gitignore

---
+++ code/door.py
@@ -0,0 +1,8 @@
+class Door:
+    def __init__(self, x, y, image):
+        self.x = x
+        self.y = y
+        self.image = image
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
--- code/main.py
+++ code/main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 import pygame
 
+from coin import Coin
+from door import Door
+from monster import Monster
+from robot import Robot
+
 class RobotGame:
     def __init__(self):
         pygame.init()
@@ -84,56 +89,6 @@ class RobotGame:
         self.coin.draw(self.window)
         self.monster.draw(self.window)
         self.door.draw(self.window)
-
-class Robot:
-    def __init__(self, x, y, image):
-        self.x = x
-        self.y = y
-        self.image = image
-        self.to_left = False
-        self.to_right = False
-
-    def set_movement(self, direction, is_pressed):
-        if direction == "left":
-            self.to_left = is_pressed
-        if direction == "right":
-            self.to_right = is_pressed
-
-    def update(self, window_width):
-        if self.to_left and self.x > 0:
-            self.x -= 5
-        if self.to_right and self.x < window_width - self.image.get_width():
-            self.x += 5
-
-    def draw(self, surface):
-        surface.blit(self.image, (self.x, self.y))
-
-class Coin:
-    def __init__(self, x, y, image):
-        self.x = x
-        self.y = y
-        self.image = image
-
-    def draw(self, surface):
-        surface.blit(self.image, (self.x, self.y))
-
-class Monster:
-    def __init__(self, x, y, image):
-        self.x = x
-        self.y = y
-        self.image = image
-
-    def draw(self, surface):
-        surface.blit(self.image, (self.x, self.y))
-
-class Door:
-    def __init__(self, x, y, image):
-        self.x = x
-        self.y = y
-        self.image = image
-
-    def draw(self, surface):
-        surface.blit(self.image, (self.x, self.y))
 
 if __name__ == "__main__":
     RobotGame()
