dee82b7a86792cee0c74269b8994db9d21aa7cf9
joaonunovalente <jnvalente@ua.pt>
2025-08-16 14:13:04 +0100

    Add movement to the Robot

--- code/main.py
+++ code/main.py
@@ -4,35 +4,75 @@ import pygame
 class RobotGame:
     def __init__(self):
         pygame.init()
-        
         self.load_images()
-        
+
         self.height = 1080
         self.width = 1920
         self.scale = 1
-
         window_height = self.scale * self.height
         window_width = self.scale * self.width
         self.window = pygame.display.set_mode((window_width, window_height))
-
         pygame.display.set_caption("Robot Game")
 
+        # Initialize objects and pass the loaded images
+        self.robot = Robot(self.width // 2, self.height - self.images["robot"].get_height(), self.images["robot"])
+        self.coin = Coin(300, 300, self.images["coin"])
+        self.monster = Monster(400, 400, self.images["monster"])
+        self.door = Door(500, 500, self.images["door"])
         self.main_loop()
 
     def load_images(self):
-        self.images = []
+        self.images = {}
         for name in ["door", "coin", "robot", "monster"]:
-            self.images.append(pygame.image.load("assets/" + name + ".png"))
+            try:
+                self.images[name] = pygame.image.load("assets/" + name + ".png")
+            except FileNotFoundError:
+                print(f"Error: Could not load image {name}.png")
+                pygame.quit()
+                exit()
 
     def main_loop(self):
         while True:
             self.check_events()
+            self.update()
             self.draw_window()
 
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
+            # Keyboard controls
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.robot.set_movement("left", True)
+                if event.key == pygame.K_RIGHT:
+                    self.robot.set_movement("right", True)
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    self.robot.set_movement("left", False)
+                if event.key == pygame.K_RIGHT:
+                    self.robot.set_movement("right", False)
+            # Touch/mobile controls
+            elif event.type == pygame.FINGERDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                # Get the position of the touch/click
+                if event.type == pygame.FINGERDOWN:
+                    x, y = event.x * self.width, event.y * self.height
+                else:
+                    x, y = pygame.mouse.get_pos()
+
+                # Split the screen into three vertical columns
+                column_width = self.width // 3
+                if x < column_width:  # Left column
+                    self.robot.set_movement("left", True)
+                elif x > 2 * column_width:  # Right column
+                    self.robot.set_movement("right", True)
+            elif event.type == pygame.FINGERUP or event.type == pygame.MOUSEBUTTONUP:
+                # Stop movement when finger/mouse is released
+                self.robot.set_movement("left", False)
+                self.robot.set_movement("right", False)
+
+    def update(self):
+        self.robot.update(self.width)
 
     def draw_window(self):
         self.window.fill((230, 230, 230))
@@ -40,50 +80,57 @@ class RobotGame:
         pygame.display.flip()
 
     def draw_objects(self):
-        self.robot = Robot(200, 200)
         self.robot.draw(self.window)
-
-        self.coin = Coin(300, 300)
         self.coin.draw(self.window)
-
-        self.monster = Monster(400, 400)
         self.monster.draw(self.window)
-
-        self.door = Door(500, 500)
         self.door.draw(self.window)
 
 class Robot:
-    def __init__(self, x, y):
+    def __init__(self, x, y, image):
         self.x = x
         self.y = y
-        self.image = pygame.image.load("assets/robot.png")
+        self.image = image
+        self.to_left = False
+        self.to_right = False
+
+    def set_movement(self, direction, is_pressed):
+        if direction == "left":
+            self.to_left = is_pressed
+        if direction == "right":
+            self.to_right = is_pressed
+
+    def update(self, window_width):
+        if self.to_left and self.x > 0:
+            self.x -= 5
+        if self.to_right and self.x < window_width - self.image.get_width():
+            self.x += 5
 
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
 
 class Coin:
-    def __init__(self, x, y):
+    def __init__(self, x, y, image):
         self.x = x
         self.y = y
-        self.image = pygame.image.load("assets/coin.png")
+        self.image = image
 
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
 
 class Monster:
-    def __init__(self, x, y):
+    def __init__(self, x, y, image):
         self.x = x
         self.y = y
-        self.image = pygame.image.load("assets/monster.png")
+        self.image = image
 
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
 
 class Door:
-    def __init__(self, x, y):
+    def __init__(self, x, y, image):
         self.x = x
         self.y = y
-        self.image = pygame.image.load("assets/door.png")
+        self.image = image
 
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
